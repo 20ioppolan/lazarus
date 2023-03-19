57050f2234d140ce365044b52bc1d461dae06035
Anthony Ioppolo <20ioppolan@gmail.com>
2023-03-19 18:32:35 -0400

    can embed files now + files

--- evil.go
+++ evil.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"embed"
+	_ "embed"
+	"fmt"
+)
+
+var (
+	//go:embed files
+	content embed.FS
+)
+
+func main() {
+	entries, err := content.ReadDir("files")
+	if err != nil {
+		panic(err)
+	}
+
+	for _, entry := range entries {
+		if !entry.IsDir() {
+			fmt.Println(entry.Name())
+		}
+	}
+}
