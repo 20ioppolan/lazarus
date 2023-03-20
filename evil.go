a10a09c9fc1a4e0520a32fca7d0da88f679d52bc
Anthony Ioppolo <20ioppolan@gmail.com>
2023-03-20 10:35:54 -0400

    switching to in-memory

--- evil.go
+++ evil.go
@@ -1,25 +1,15 @@
 package main
 
 import (
-	"embed"
 	_ "embed"
 	"fmt"
 )
 
 var (
-	//go:embed files
-	content embed.FS
+	//go:embed files/locations.txt
+	locations string
 )
 
 func main() {
-	entries, err := content.ReadDir("files")
-	if err != nil {
-		panic(err)
-	}
-
-	for _, entry := range entries {
-		if !entry.IsDir() {
-			fmt.Println(entry.Name())
-		}
-	}
+	fmt.Println(locations)
 }
